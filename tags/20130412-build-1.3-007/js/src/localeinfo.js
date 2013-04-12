3f1133fecb55efad9d61bdce7fbfb1a693b1cd58
ehoogerbeets1 <ehoogerbeets1@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2013-04-12 08:53:45 +0000

    build 7

---
+++ tags/20130412-build-1.3-007/js/src/localeinfo.js
@@ -0,0 +1,338 @@
+/*
+ * localeinfo.js - Encode locale-specific defaults
+ * 
+ * Copyright © 2012-2013, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// !depends ilibglobal.js locale.js
+
+// !data localeinfo likelylocales
+
+/**
+ * @class
+ * Create a new locale info instance. Locale info instances give information about
+ * the default settings for a particular locale. These settings may be overridden
+ * by various parts of the code, and should be used as a fall-back setting of last
+ * resort. <p>
+ * 
+ * The optional options object holds extra parameters if they are necessary. The
+ * current list of supported options are:
+ * 
+ * <ul>
+ * <li>onLoad - a callback function to call when the locale info object is fully 
+ * loaded. When the onLoad option is given, the localeinfo object will attempt to
+ * load any missing locale data using the ilib loader callback.
+ * When the constructor is done (even if the data is already preassembled), the 
+ * onLoad function is called with the current instance as a parameter, so this
+ * callback can be used with preassembled or dynamic loading or a mix of the two.
+ * 
+ * <li>sync - tell whether to load any missing locale data synchronously or 
+ * asynchronously. If this option is given as "false", then the "onLoad"
+ * callback must be given, as the instance returned from this constructor will
+ * not be usable for a while. 
+ * </ul>
+ * 
+ * If this copy of ilib is pre-assembled and all the data is already available, 
+ * or if the data was already previously loaded, then this constructor will call
+ * the onLoad callback immediately when the initialization is done. 
+ * If the onLoad option is not given, this class will only attempt to load any
+ * missing locale data synchronously.
+ * 
+ * Depends directive: !depends localeinfo.js
+ * 
+ * @constructor
+ * @see {ilib.setLoaderCallback} for information about registering a loader callback
+ * function
+ * @param {ilib.Locale|string=} locale the locale for which the info is sought, or undefined for
+ * @param {Object=} options the locale for which the info is sought, or undefined for
+ * the current locale
+ */
+ilib.LocaleInfo = function(locale, options) {
+	var sync = true;
+	
+	/* these are all the defaults. Essentially, en-US */
+	this.info = ilib.data.localeinfo;
+	
+	switch (typeof(locale)) {
+		case "string":
+			this.locale = new ilib.Locale(locale);
+			break;
+		default:
+		case "undefined":
+			this.locale = new ilib.Locale();
+			break;
+		case "object":
+			this.locale = locale;
+			break;
+	}
+	
+	if (options) {
+		if (typeof(options.sync) !== 'undefined') {
+			sync = (options.sync == true);
+		}
+	}
+
+	if (!ilib.LocaleInfo.cache) {
+		ilib.LocaleInfo.cache = {};
+	}
+
+	var spec = this.locale.getSpec().replace(/-/g, "_");
+	if (typeof(ilib.LocaleInfo.cache[spec]) === 'undefined') {
+		this.info = ilib.mergeLocData("localeinfo", this.locale);
+		if (this.info) {
+			ilib.LocaleInfo.cache[spec] = this.info;
+			if (options && typeof(options.onLoad) === 'function') {
+				options.onLoad(this);
+			}
+		} else if (typeof(ilib._load) === 'function') {
+			// locale is not preassembled, so attempt to load it dynamically
+			var files = ilib.getLocFiles(this.locale, "localeinfo");
+			
+			ilib._load(files, sync, ilib.bind(this, function(arr) {
+				this.info = {};
+				for (var i = 0; i < arr.length; i++) {
+					if (typeof(arr[i]) !== 'undefined') {
+						this.info = ilib.merge(this.info, arr[i]);
+					}
+				}
+	
+				ilib.LocaleInfo.cache[spec] = this.info;
+				
+				if (options && typeof(options.onLoad) === 'function') {
+					options.onLoad(this);
+				}
+			}));
+		} else {
+			// no data other than the generic shared data
+			this.info = ilib.data.localeinfo;
+			ilib.LocaleInfo.cache[spec] = this.info;
+			if (options && typeof(options.onLoad) === 'function') {
+				options.onLoad(this);
+			}
+		}
+	} else {
+		this.info = ilib.LocaleInfo.cache[spec];
+		if (options && typeof(options.onLoad) === 'function') {
+			options.onLoad(this);
+		}
+	}
+};
+
+ilib.LocaleInfo.prototype = {
+	/**
+	 * Return whether this locale commonly uses the 12- or the 24-hour clock.
+	 *  
+	 * @returns {string} "12" if the locale commonly uses a 12-hour clock, or "24"
+	 * if the locale commonly uses a 24-hour clock. 
+	 */
+	getClock: function() {
+		return this.info.clock;
+	},
+
+	/**
+	 * Return the locale that this info object was created with.
+	 * @returns {ilib.Locale} The locale spec of the locale used to construct this info instance
+	 */
+	getLocale: function () {
+		return this.locale;
+	},
+	
+	/**
+	 * Return the name of the measuring system that is commonly used in the given locale.
+	 * Valid values are "uscustomary", "imperial", and "metric".
+	 * 
+	 * @returns {string} The name of the measuring system commonly used in the locale
+	 */
+	getUnits: function () {
+		return this.info.units;
+	},
+	
+	/**
+	 * Return the name of the calendar that is commonly used in the given locale.
+	 * 
+	 * @returns {string} The name of the calendar commonly used in the locale
+	 */
+	getCalendar: function () {
+		return this.info.calendar;
+	},
+	
+	/**
+	 * Return the day of week that starts weeks in the current locale. Days are still
+	 * numbered the standard way with 0 for Sunday through 6 for Saturday, but calendars 
+	 * should be displayed and weeks calculated with the day of week returned from this 
+	 * function as the first day of the week.
+	 * 
+	 * @returns {number} the day of the week that starts weeks in the current locale.
+	 */
+	getFirstDayOfWeek: function () {
+		return this.info.firstDayOfWeek;
+	},
+	
+	/**
+	 * Return the default time zone for this locale. Many locales span across multiple
+	 * time zones. In this case, the time zone with the largest population is chosen
+	 * to represent the locale. This is obviously not that accurate, but then again,
+	 * this method's return value should only be used as a default anyways.
+	 * @returns {string} the default time zone for this locale.
+	 */
+	getTimeZone: function () {
+		return this.info.timezone;
+	},
+	
+	/**
+	 * Return the decimal separator for formatted numbers in this locale.
+	 * @returns {string} the decimal separator char
+	 */
+	getDecimalSeparator: function () {
+		return this.info.numfmt.decimalChar;
+	},
+	
+	/**
+	 * Return the separator character used to separate groups of digits on the 
+	 * integer side of the decimal character.
+	 * @returns {string} the grouping separator char
+	 */
+	getGroupingSeparator: function () {
+		return this.info.numfmt.groupChar;
+	},
+	
+	/**
+	 * Return the minimum number of digits grouped together on the integer side. 
+	 * In western European cultures, groupings are in 1000s, so the number of digits
+	 * is 3. In other cultures, the groupings are in 10000s so the number is 4.
+	 * @returns {number} the number of digits in a grouping, or 0 for no grouping
+	 */
+	getGroupingDigits: function () {
+		return this.info.numfmt.groupSize;
+	},
+	
+	/**
+	 * Return the format template used to format percentages in this locale.
+	 * @returns {string} the format template for formatting percentages
+	 */
+	getPercentageFormat: function () {
+		return this.info.numfmt.pctFmt;
+	},
+	
+	/**
+	 * Return the symbol used for percentages in this locale.
+	 * @returns {string} the symbol used for percentages in this locale
+	 */
+	getPercentageSymbol: function () {
+		return this.info.numfmt.pctChar || "%";
+	},
+
+	/**
+	 * Return an object containing the format templates for formatting currencies
+	 * in this locale. The object has a number of properties in it that each are
+	 * a particular style of format. Normally, this contains a "common" and an "iso"
+	 * style, but may contain others in the future.
+	 * @returns {Object} an object containing the format templates for currencies
+	 */
+	getCurrencyFormats: function () {
+		return this.info.currencyFormats;
+	},
+
+	/**
+	 * Return the currency that is legal in the locale, or which is most commonly 
+	 * used in regular commerce.
+	 * @returns {string} the ISO 4217 code for the currency of this locale
+	 */
+	getCurrency: function () {
+		return this.info.currency;
+	},
+	
+	/**
+	 * If this locale typically uses a different type of rounding for numeric
+	 * formatting other than halfdown, especially for currency, then it can be 
+	 * specified in the localeinfo. If the locale uses the default, then this 
+	 * method returns undefined. The locale's rounding method overrides the 
+	 * rounding method for the currency itself, which can sometimes shared 
+	 * between various locales so it is less specific.
+	 * @returns {string} the name of the rounding mode typically used in this
+	 * locale, or "halfdown" if the locale does not override the default
+	 */
+	getRoundingMode: function () {
+		return this.info.numfmt.roundingMode;
+	},
+	
+	/**
+	 * Return the default script used to write text in the language of this 
+	 * locale. Text for most languages is written in only one script, but there
+	 * are some languages where the text can be written in a number of scripts,
+	 * depending on a variety of things such as the region, ethnicity, religion, 
+	 * etc. of the author. This method returns the default script for the
+	 * locale, in which the language is most commonly written.<p> 
+	 * 
+	 * The script is returned as an ISO 15924 4-letter code.
+	 * 
+	 * @returns {string} the ISO 15924 code for the default script used to write
+	 * text in this locale 
+	 */
+	getDefaultScript: function() {
+		return (this.info.scripts) ? this.info.scripts[0] : "Latn";
+	},
+	
+	/**
+	 * Return the script used for the current locale. If the current locale
+	 * explicitly defines a script, then this script is returned. If not, then 
+	 * the default script for the locale is returned.
+	 * 
+	 * @see ilib.LocaleInfo.getDefaultScript
+	 * @returns {string} the ISO 15924 code for the script used to write
+	 * text in this locale
+	 */
+	getScript: function() {
+		return this.locale.getScript() || this.getDefaultScript(); 
+	},
+	
+	/**
+	 * Return an array of script codes which are used to write text in the current
+	 * language. Text for most languages is written in only one script, but there
+	 * are some languages where the text can be written in a number of scripts,
+	 * depending on a variety of things such as the region, ethnicity, religion, 
+	 * etc. of the author. This method returns an array of script codes in which 
+	 * the language is commonly written.
+	 * 
+	 * @returns {Array.<string>} an array of ISO 15924 codes for the scripts used 
+	 * to write text in this language
+	 */
+	getAllScripts: function() {
+		return this.info.scripts || ["Latn"];
+	},
+	
+	/**
+	 * Return an ilib.Locale instance that is fully specified based on partial information
+	 * given to the constructor of this locale info instance. For example, if the locale
+	 * spec given to this locale info instance is simply "ru" (for the Russian language), 
+	 * then it will fill in the missing region and script tags and return a locale with 
+	 * the specifier "ru-RU-Cyrl". (ie. Russian language, Russian Federation, Cyrillic).
+	 * Any one or two of the language, script, or region parts may be left unspecified,
+	 * and the other one or two parts will be filled in automatically. If this
+	 * class has no information about the given locale, then the locale of this
+	 * locale info instance is returned unchanged.
+	 * 
+	 * @returns {ilib.Locale} the most likely completion of the partial locale given
+	 * to the constructor of this locale info instance
+	 */
+	getLikelyLocale: function () {
+		if (typeof(ilib.data.likelylocales[this.locale.getSpec()]) === 'undefined') {
+			return this.locale;
+		}
+		
+		return new ilib.Locale(ilib.data.likelylocales[this.locale.getSpec()]);
+	}
+};
