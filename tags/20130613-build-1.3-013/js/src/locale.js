5faf48bf48f063925b0be24d93068b2883ea8e15
ehoogerbeets1 <ehoogerbeets1@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2013-06-14 06:16:49 +0000

    build 13

---
+++ tags/20130613-build-1.3-013/js/src/locale.js
@@ -0,0 +1,355 @@
+/*
+ * locale.js - Locale specifier definition
+ * 
+ * Copyright © 2012, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// !depends ilibglobal.js
+
+/**
+ * @class
+ * Create a new locale instance. Locales are specified either with a specifier string 
+ * that follows the BCP-47 convention (roughly: "language-region-script-variant") or 
+ * with 4 parameters that specify the language, region, variant, and script individually.<p>
+ * 
+ * The language is given as an ISO 639-1 two-letter, lower-case language code. You
+ * can find a full list of these codes at 
+ * <a href="http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes">http://en.wikipedia.org/wiki/List_of_ISO_639-1_codes</a><p>
+ * 
+ * The region is given as an ISO 3166-1 two-letter, upper-case region code. You can
+ * find a full list of these codes at 
+ * <a href="http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2">http://en.wikipedia.org/wiki/ISO_3166-1_alpha-2</a>.<p>
+ * 
+ * The variant is any string that does not contain a dash which further differentiates
+ * locales from each other.<p>
+ * 
+ * The script is given as the ISO 15924 four-letter script code. In some locales,
+ * text may be validly written in more than one script. For example, Serbian is often
+ * written in both Latin and Cyrillic, though not usually mixed together. You can find a
+ * full list of these codes at 
+ * <a href="http://en.wikipedia.org/wiki/ISO_15924#List_of_codes">http://en.wikipedia.org/wiki/ISO_15924#List_of_codes</a>.<p>
+ * 
+ * As an example in ilib, the script can be used in the date formatter. Dates formatted 
+ * in Serbian could have day-of-week names or month names written in the Latin
+ * or Cyrillic script. Often one script is default such that sr-SR-Latn is the same
+ * as sr-SR so the script code "Latn" can be left off of the locale spec.<p> 
+ * 
+ * Each part is optional, and an empty string in the specifier before or after a 
+ * dash or as a parameter to the constructor denotes an unspecified value. In this
+ * case, many of the ilib functions will treat the locale as generic. For example
+ * the locale "en-" is equivalent to "en" and to "en--" and denotes a locale
+ * of "English" with an unspecified region and variant, which typically matches
+ * any region or variant.<p>
+ * 
+ * Without any arguments to the constructor, this function returns the locale of
+ * the host Javascript engine.<p>
+ * 
+ * Depends directive: !depends locale.js
+ * 
+ * @constructor
+ * @param {?string=} language the ISO 639 2-letter code for the language, or a full 
+ * locale spec in BCP-47 format
+ * @param {string=} region the ISO 3166 2-letter code for the region
+ * @param {string=} variant the name of the variant of this locale, if any
+ * @param {string=} script the ISO 15924 code of the script for this locale, if any
+ */
+ilib.Locale = function(language, region, variant, script) {
+	if (typeof(region) === 'undefined') {
+		var spec = language || ilib.getLocale();
+		var parts = spec.split('-');
+        for ( var i = 0; i < parts.length; i++ ) {
+        	if (ilib.Locale._isLanguageCode(parts[i])) {
+    			/** 
+    			 * @private
+    			 * @type {string|undefined}
+    			 */
+        		this.language = parts[i];
+        	} else if (ilib.Locale._isRegionCode(parts[i])) {
+    			/** 
+    			 * @private
+    			 * @type {string|undefined}
+    			 */
+        		this.region = parts[i];
+        	} else if (ilib.Locale._isScriptCode(parts[i])) {
+    			/** 
+    			 * @private
+    			 * @type {string|undefined}
+    			 */
+        		this.script = parts[i];
+        	} else {
+    			/** 
+    			 * @private
+    			 * @type {string|undefined}
+    			 */
+        		this.variant = parts[i];
+        	}
+        }
+        this.language = this.language || undefined;
+        this.region = this.region || undefined;
+        this.script = this.script || undefined;
+        this.variant = this.variant || undefined;
+	} else {
+		if (language) {
+			language = language.trim();
+			this.language = language.length > 0 ? language.toLowerCase() : undefined;
+		} else {
+			this.language = undefined;
+		}
+		if (region) {
+			region = region.trim();
+			this.region = region.length > 0 ? region.toUpperCase() : undefined;
+		} else {
+			this.region = undefined;
+		}
+		if (variant) {
+			variant = variant.trim();
+			this.variant = variant.length > 0 ? variant : undefined;
+		} else {
+			this.variant = undefined;
+		}
+		if (script) {
+			script = script.trim();
+			this.script = script.length > 0 ? script : undefined;
+		} else {
+			this.script = undefined;
+		}
+	}
+	this.spec = this.language || "";
+	
+	if (this.script) {
+		if (this.spec.length > 0) {
+			this.spec += "-";
+		}
+		this.spec += this.script;
+	}
+	
+	if (this.region) {
+		if (this.spec.length > 0) {
+			this.spec += "-";
+		}
+		this.spec += this.region;
+	}
+	
+	if (this.variant) {
+		if (this.spec.length > 0) {
+			this.spec += "-";
+		}
+		this.spec += this.variant;
+	}
+};
+
+/**
+ * @private
+ * Tell whether or not the str does not start with a lower case ASCII char.
+ * @param {string} str the char to check
+ * @return {boolean} true if the char is not a lower case ASCII char
+ */
+ilib.Locale._notLower = function(str) {
+	// do this with ASCII only so we don't have to depend on the CType functions
+	var ch = str.charCodeAt(0);
+	return ch < 97 || ch > 122;
+};
+
+/**
+ * @private
+ * Tell whether or not the str does not start with an upper case ASCII char.
+ * @param {string} str the char to check
+ * @return {boolean} true if the char is a not an upper case ASCII char
+ */
+ilib.Locale._notUpper = function(str) {
+	// do this with ASCII only so we don't have to depend on the CType functions
+	var ch = str.charCodeAt(0);
+	return ch < 65 || ch > 90;
+};
+
+/**
+ * @private
+ * Tell whether or not the str does not start with a digit char.
+ * @param {string} str the char to check
+ * @return {boolean} true if the char is a not an upper case ASCII char
+ */
+ilib.Locale._notDigit = function(str) {
+	// do this with ASCII only so we don't have to depend on the CType functions
+	var ch = str.charCodeAt(0);
+	return ch < 48 || ch > 57;
+};
+
+/**
+ * @private
+ * Tell whether or not the given string has the correct syntax to be 
+ * an ISO 639 language code.
+ * 
+ * @param {string} str the string to parse
+ * @return {boolean} true if the string could syntactically be a language code.
+ */
+ilib.Locale._isLanguageCode = function(str) {
+	if (typeof(str) === 'undefined' || str.length < 2 || str.length > 3) {
+		return false;
+	}
+
+	for (var i = 0; i < str.length; i++) {
+		if (ilib.Locale._notLower(str.charAt(i))) {
+			return false;
+		}
+	}
+	
+	return true;
+};
+
+/**
+ * @private
+ * Tell whether or not the given string has the correct syntax to be 
+ * an ISO 3166 2-letter region code or M.49 3-digit region code.
+ * 
+ * @param {string} str the string to parse
+ * @return {boolean} true if the string could syntactically be a language code.
+ */
+ilib.Locale._isRegionCode = function (str) {
+	if (typeof(str) === 'undefined' || str.length < 2 || str.length > 3) {
+		return false;
+	}
+	
+	if (str.length === 2) {
+		for (var i = 0; i < str.length; i++) {
+			if (ilib.Locale._notUpper(str.charAt(i))) {
+				return false;
+			}
+		}
+	} else {
+		for (var i = 0; i < str.length; i++) {
+			if (ilib.Locale._notDigit(str.charAt(i))) {
+				return false;
+			}
+		}
+	}
+	
+	return true;
+};
+
+/**
+ * @private
+ * Tell whether or not the given string has the correct syntax to be 
+ * an ISO 639 language code.
+ * 
+ * @param {string} str the string to parse
+ * @return {boolean} true if the string could syntactically be a language code.
+ */
+ilib.Locale._isScriptCode = function(str)
+{
+	if (typeof(str) === 'undefined' || str.length !== 4 || ilib.Locale._notUpper(str.charAt(0))) {
+		return false;
+	}
+	
+	for (var i = 1; i < 4; i++) {
+		if (ilib.Locale._notLower(str.charAt(i))) {
+			return false;
+		}
+	}
+	
+	return true;
+};
+
+ilib.Locale.prototype = {
+	/**
+	 * Return the ISO 639 language code for this locale. 
+	 * @return {string|undefined} the language code for this locale 
+	 */
+	getLanguage: function() {
+		return this.language;
+	},
+	
+	/**
+	 * Return the ISO 3166 region code for this locale.
+	 * @return {string|undefined} the region code of this locale
+	 */
+	getRegion: function() {
+		return this.region;
+	},
+	
+	/**
+	 * Return the ISO 15924 script code for this locale
+	 * @return {string|undefined} the script code of this locale
+	 */
+	getScript: function () {
+		return this.script;
+	},
+	
+	/**
+	 * Return the variant code for this locale
+	 * @return {string|undefined} the variant code of this locale, if any
+	 */
+	getVariant: function() {
+		return this.variant;
+	},
+	
+	/**
+	 * Return the whole locale specifier as a string.
+	 * @return {string} the locale specifier
+	 */
+	getSpec: function() {
+		return this.spec;
+	},
+	
+	/**
+	 * Express this locale object as a string. Currently, this simply calls the getSpec
+	 * function to represent the locale as its specifier.
+	 * 
+	 * @return {string} the locale specifier
+	 */
+	toString: function() {
+		return this.getSpec();
+	},
+	
+	/**
+	 * Return true if the the other locale is exactly equal to the current one.
+	 * @return {boolean} whether or not the other locale is equal to the current one 
+	 */
+	equals: function(other) {
+		return this.language === other.language &&
+			this.region === other.region &&
+			this.script === other.script &&
+			this.variant === other.variant;
+	},
+
+	/**
+	 * Return true if the current locale is the special pseudo locale.
+	 * @return {boolean} true if the current locale is the special pseudo locale
+	 */
+	isPseudo: function () {
+		return (this.language === 'zxx' && this.region === 'XX');
+	}
+};
+
+// static functions
+/**
+ * @private
+ */
+ilib.Locale.locales = [
+	// !macro localelist
+];
+
+/**
+ * Return the list of available locales that this iLib file was assembled
+ * with. The list that this file was assembled with may be much smaller
+ * than the list of all available locales in the iLib repository. The
+ * assembly tool will automatically fill in the list.
+ * 
+ * @return {Array.<string>} this is an array of locale specs for which 
+ * this iLib file has locale data for
+ */
+ilib.Locale.getAvailableLocales = function () {
+	return ilib.Locale.locales;
+};
