c006c3ab6825f78dfcb14a046ed5f7571367f224
shashi009kant <shashi009kant@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2013-06-24 06:02:59 +0000

    this code helps to remove non json file which is present in js/data/locale directory

---
+++ tools/cldr/node.js
@@ -0,0 +1,105 @@
+var fs = require('fs');
+var util = require('util');
+var common = require('./common');
+
+
+function usage() {
+	util.print("Usage: gennumfmts [-h] CLDR_json_dir locale_data_dir\n" +
+		"Generate number formats information files.\n\n" +
+		"-h or --help\n" +
+		"  this help\n" +
+		"locale_data_dir\n" +
+		"  the top level of the ilib locale data directory\n");
+	process.exit(1);
+}
+
+
+
+
+var walk = function(dir) {
+    var results = []
+    var list = fs.readdirSync(dir)
+    list.forEach(function(file) {
+	
+     	file = dir + '/' + file
+	var merged = {};
+        var stat = fs.statSync(file)
+        if (stat && stat.isDirectory()) (walk(file))
+	
+        else {
+		var obj={};
+		(file);
+		(typeof(file));
+		
+	 		if(file.match(/[a-z]+\.jf/)){
+				
+				var data = fs.readFileSync(file,'utf8');
+					
+					 /* if (err) {
+						fs.unlink(file);
+						("MMMMMMMMMMMMMMMMMMM");
+						file='[]';
+						throw err;
+					}*/
+					
+					try {	
+  					  obj = JSON.parse(data);
+						
+						}
+					 
+
+
+					catch(err){
+						fs.unlink(file);
+						
+						file='[]';
+				     }
+	
+					
+			
+			
+				if(file != '[]'){
+				
+				(file);
+				results.push(file);
+				}
+				}
+			
+	
+	}
+	
+	
+	
+	
+      })
+ 
+	var merged = {}
+for (i = 0; i < results.length; i++) {
+	var json = fs.readFileSync(results[i],'utf8');
+	var root = JSON.parse(json);
+	merged = common.merge(merged, root);
+}
+var path = dir;
+ (merged);
+if (!fs.existsSync(path)) {
+		fs.mkdirSync(path);
+		}
+if(Object.keys(merged).length!=0) {
+	    fs.writeFileSync(path + "/localeinfo.json",JSON.stringify(merged),'utf8');
+	}  
+	return results;
+}
+if (process.argv.length < 3) {
+	util.print('Error: not enough arguments');
+	usage();
+}
+
+localeDirName = process.argv[2];
+util.print("locale dir: " + localeDirName + "\n");
+fs.exists(localeDirName, function (exists) {
+		if (!exists) {
+			util.print("Could not access locale data directory " + localeDirName);
+			usage();
+		}
+	});
+walk(localeDirName);
