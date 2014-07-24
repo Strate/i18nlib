e8e679462e3453c8e90373536396e1d46275dba1
ehoogerbeets1 <ehoogerbeets1@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2014-07-24 18:35:32 +0000

    Charmaps are now generated correctly based on the linux charmaps in /usr/share/i18n/charmaps. Added a few more aliases for missing charmap names.

--- branches/charset/tools/cldr/gencharmap.js
+++ branches/charset/tools/cldr/gencharmap.js
@@ -26,25 +26,28 @@ var fs = require('fs');
 var util = require('util');
 var unifile = require('./unifile.js');
 var common = require('./common.js');
+var CharmapFile = require('./charmapfile.js').CharmapFile;
 var path = require('path');
-var UnicodeFile = unifile.UnicodeFile;
 var charIterator = common.charIterator;
 var isMember = common.isMember;
 
 function usage() {
-	util.print("Usage: gencharmap [-h] path_to_UCD [toDir]\n" +
+	util.print("Usage: gencharmap [-h] path_to_charmaps charsetaliases.json [toDir]\n" +
 			"Generate the character type data.\n\n" +
 			"-h or --help\n" +
 			"  this help\n" +
-			"path_to_UCDE\n" +
-			"  path to the Unicode Character Data directory containing the MAPPINGS dir\n" +
+			"path_to_charmaps\n" +
+			"  path to the charmap directory containing the charset mappings\n" +
+			"charsealiases.json\n" +
+			"  path to json file with the mappings to standard IANA charset names\n" +
 			"toDir\n" +
 			"  directory to output the normalization json files. Default: <currentdir>/charmaps.\n");
 	process.exit(1);
 }
 
-var ucdDir;
+var charmapDir;
 var toDir = ".";
+var aliasesFile;
 
 process.argv.forEach(function (val, index, array) {
 	if (val === "-h" || val === "--help") {
@@ -57,9 +60,12 @@ if (process.argv.length < 3) {
 	usage();
 }
 
-ucdDir = path.join(process.argv[2], "MAPPINGS");
-if (process.argv.length > 3) {
-	toDir = process.argv[3];
+charmapDir = process.argv[2];
+
+aliasesFile = process.argv[3];
+
+if (process.argv.length > 4) {
+	toDir = process.argv[4];
 }
 
 toDir = path.join(toDir, "charmaps");
@@ -67,8 +73,13 @@ toDir = path.join(toDir, "charmaps");
 util.print("gencharmap - generate charmap mapping data.\n" +
 		"Copyright (c) 2014 JEDLSoft\n");
 
-if (!fs.existsSync(ucdDir)) {
-	util.error("Could not access dir " + ucdDir);
+if (!fs.existsSync(charmapDir)) {
+	util.error("Could not access dir " + charmapDir);
+	usage();
+}
+
+if (!fs.existsSync(aliasesFile)) {
+	util.error("Could not access aliases file " + aliasesFile);
 	usage();
 }
 
@@ -79,51 +90,91 @@ if (!fs.existsSync(toDir)) {
 	}
 }
 
+var aliasesData = fs.readFileSync(aliasesFile);
+var aliases = JSON.parse(aliasesData);
+
 var map = {};
 
 function walk(dir) {
 	var list = fs.readdirSync(dir);
 	list.forEach(function (file) {
 		var fullpath = dir + '/' + file;
+		// util.print("fullpath is " + fullpath + "\n");
 		var stat = fs.statSync(fullpath);
 		if (stat && stat.isDirectory()) {
 			walk(fullpath);
 		} else {
-			if (fullpath.match(/[a-z]+\.txt$/)) {
+			//if (fullpath.match(/.gz$/)) {
+				// util.print("found charmap file " + fullpath + "\n");
 				try {
-					var toFileName = path.join(toDir, path.basename(file, ".txt") + ".json");
-					
+					var fileName = aliases[file.replace(/[-_:\+\.\(\)]/g, '').toUpperCase()] || file;
+					var toFileName = path.join(toDir, fileName + ".json");
+					// util.print("Writing results to file " + toFileName + "\n");
 					var map = {
 						to: {},
 						from: {}
 					};
-					var uf = new UnicodeFile({
-						path: fullpath,
-						splitChar: /\s+/
-					});
+					var uf = new CharmapFile({path: fullpath});
 					var len = uf.length();
 					var native, unicode;
+					var uniRE = /^<U([0-9a-fA-F]+)>/;
+					var mapRE = /^\/x/;
 						
-					for (var i = 0; i < len; i++ ) {
+					// util.print("len is " + len + "\n");
+					for (var i = 0; i < len; i++) {
 						row = uf.get(i);
 
-						util.print("row is " + JSON.stringify(row) + "\n");
-						native = parseInt(row[0], 16);
-						unicode = parseInt(row[1], 16);
-						map.to[native] = unicode;
-						map.from[unicode] = native;
+						// util.print("row is " + JSON.stringify(row) + "\n");
+						var uniResult = uniRE.exec(row[0]);
+						if (uniResult !== null && mapRE.test(row[1])) {
+							nativeByteStrings = row[1].split(/\/x/g);
+							native = []; 
+							for (var j = 0; j < nativeByteStrings.length; j++) {
+								if (nativeByteStrings[j].length > 0) {
+									//native <<= 8;
+									//native = native | parseInt(nativeByteStrings[j], 16);
+									native.push(parseInt(nativeByteStrings[j], 16));
+								}
+							}
+							unicode = parseInt(uniResult[1], 16);
+							if (unicode < 32) {
+								unicode = String.fromCharCode(0x5C) + "u" + row[1].substring(2);
+							} else {
+								unicode = String.fromCharCode(unicode);	
+							}
+							if (native.length > 0 && unicode) {
+								var trienode = map.to;
+								for (var j = 0; j < native.length-1; j++) {
+									if (typeof(trienode[native[j]]) === 'undefined') {
+										trienode[native[j]] = {};
+									}
+									
+									trienode = trienode[native[j]];
+								}
+								trienode[native[j]] = unicode;
+								map.from[unicode] = native;	
+							} else {
+								util.print("skipping bad mapping from " + row[0] + " to " + row[1] + "\n");
+							}
+						} else {
+							util.print("row " + row + " did not pass the test\n");
+							util.print("uniRE.exec(row[0]) was " + JSON.stringify(uniRE.exec(row[0])) + "\n");
+							util.print("mapRE.test(row[1]) was " + mapRE.test(row[1]) + "\n");
+						}
 					}
 					
-					if (fs.writeFileSync(toFileName, JSON.stringify(map, undefined, 4), "utf-8") < 0) {
-						util.print("Could not write to output file " + toFileName + "\n");
+					if (!common.isEmpty(map.to)) {
+						if (fs.writeFileSync(toFileName, JSON.stringify(map, undefined, 4), "utf-8") < 0) {
+							util.print("Could not write to output file " + toFileName + "\n");
+						}
 					}
 				} catch (err) {
 					util.print("File " + fullpath + " is not readable or does not contain valid unicode mapping data.\n");
 					util.print(err + "\n");
 				}
-			}
+			//}
 		}
 	});
 }
 
-walk(ucdDir);
+walk(charmapDir);
