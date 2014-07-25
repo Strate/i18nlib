818923b1b3d90204f741b915e56004143c1779da
ehoogerbeets1 <ehoogerbeets1@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2014-07-25 18:51:18 +0000

    Reworked the shift-jis tests to be driven by a data table instead. This will be the template for the other tests to come.
    Added an alias for "SJIS" to "Shift_JIS"

--- js/src/maps/test/testcm_Shift_JIS.js
+++ js/src/maps/test/testcm_Shift_JIS.js
@@ -17,283 +17,112 @@
  * limitations under the License.
  */
 
-function testCharmapShiftJISLoadMap() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    assertEquals("Shift_JIS", cm.getName());
+var aliases = [
+	"Shift_JIS",
+	"Shift-JIS",
+	"SHIFT_JIS",
+	"SJIS"
+];
+
+function testCharmapShiftJISTestAliases() {
+	for (var i = 0; i < aliases.length; i++) {
+		var cm = new ilib.Charmap({
+			name: aliases[i]
+		});
+	    assertNotUndefined(cm);
+	    assertEquals("Shift_JIS", cm.getName());
+	}
 }
 
-function testCharmapShiftJISMapToUnicodeASCII() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    var input = new Uint8Array(14);
-    var str = "This is a test";
-    for (var i = 0; i < str.length; i++) {
-    	input[i] = str.charCodeAt(i); 
-    }
-    
-    assertEquals("This is a test", cm.mapToUnicode(input));
-}
-
-function testCharmapShiftJISMapToUnicodeHalfWidthKatakana() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    var input = new Uint8Array([
-        0xB1, // ｱ
-        0xB6, // ｶ
-        0xD3, // ﾓ
-        0xC4  // ﾄ
-	]);
-    
-    assertEquals("ｱｶﾓﾄ", cm.mapToUnicode(input));
-}
-
-function testCharmapShiftJISMapToUnicodeHiragana() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    var input = new Uint8Array([
-        0x82, 0xD0, // ひ 
-        0x82, 0xE7, // ら 
-        0x82, 0xAA, // が 
-        0x82, 0xC8, // な 
-        0x82, 0xC5, // で 
-        0x82, 0xB7  // す
-	]);
-    
-    assertEquals("ひらがなです", cm.mapToUnicode(input));
-}
-
-function testCharmapShiftJISMapToUnicodeKatakana() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    var input = new Uint8Array([
-        0x83, 0x4A, // カ
-        0x83, 0x5E, // タ
-        0x83, 0x4A, // カ 
-        0x83, 0x69, // ナ
-        0x82, 0xC5, // で 
-        0x82, 0xB7  // す
-	]);
-    
-    assertEquals("カタカナです", cm.mapToUnicode(input));
-}
-
-function testCharmapShiftJISMapToUnicodeGreek() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    var input = new Uint8Array([
-        0x83, 0xA3, // Ε
-        0x83, 0xC9, // λ
-        0x83, 0xC9, // λ 
-        0x83, 0xBF, // α
-        0x83, 0xD0  // σ 
-	]);
-    
-    assertEquals("Ελλασ", cm.mapToUnicode(input));
-}
-
-function testCharmapShiftJISMapToUnicodeCyrillic() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    var input = new Uint8Array([
-        0x84, 0x51, // Р
-        0x84, 0x85, // у
-        0x84, 0x83, // с
-        0x84, 0x83, // с
-        0x84, 0x7B, // к
-        0x84, 0x79, // и
-        0x84, 0x7A  // й
-	]);
-    
-    assertEquals("Русский", cm.mapToUnicode(input));
-}
-
-function testCharmapShiftJISMapToUnicodeKanji() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    var input = new Uint8Array([
+var testData = {
+    "This is a test": [
+		0x54, // T
+	    0x68, // h
+	    0x69, // i
+	    0x73, // s
+	    0x20, // 
+	    0x69, // i
+	    0x73, // s
+	    0x20, // 
+	    0x61, // a
+	    0x20, //
+	    0x74, // t
+	    0x65, // e
+	    0x73, // s
+	    0x74  // t
+    ],
+    "ｱｶﾓﾄ": [
+		0xB1, // ｱ
+	    0xB6, // ｶ
+	    0xD3, // ﾓ
+	    0xC4  // ﾄ
+    ],
+    "ひらがなです": [
+		0x82, 0xD0, // ひ
+	    0x82, 0xE7, // ら
+	    0x82, 0xAA, // が
+	    0x82, 0xC8, // な
+	    0x82, 0xC5, // で
+	    0x82, 0xB7  // す
+    ],
+    "カタカナです": [
+		0x83, 0x4A, // カ
+	    0x83, 0x5E, // タ
+	    0x83, 0x4A, // カ
+	    0x83, 0x69, // ナ
+	    0x82, 0xC5, // で
+	    0x82, 0xB7  // す
+    ],
+    "Ελλασ": [
+		0x83, 0xA3, // Ε
+	    0x83, 0xC9, // λ
+	    0x83, 0xC9, // λ
+	    0x83, 0xBF, // α
+	    0x83, 0xD0  // σ
+    ],
+    "Русский": [
+		0x84, 0x51, // Р
+	    0x84, 0x85, // у
+	    0x84, 0x83, // с
+	    0x84, 0x83, // с
+	    0x84, 0x7B, // к
+	    0x84, 0x79, // и
+	    0x84, 0x7A  // й
+    ],
+    "日本語は美しいです": [
 		0x93, 0xFA, // 日
-		0x96, 0x7B, // 本
-		0x8c, 0xEA, // 語
-		0x82, 0xCD, // は
-		0x94, 0xFC, // 美
-		0x82, 0xB5, // し
-		0x82, 0xA2, // い
-		0x82, 0xC5, // で
-		0x82, 0xB7  // す
-	]);
-    
-    assertEquals("日本語は美しいです", cm.mapToUnicode(input));
-}
+	    0x96, 0x7B, // 本
+	    0x8c, 0xEA, // 語
+	    0x82, 0xCD, // は
+	    0x94, 0xFC, // 美
+	    0x82, 0xB5, // し
+	    0x82, 0xA2, // い
+	    0x82, 0xC5, // で
+	    0x82, 0xB7  // す
+    ]
+};
 
-function testCharmapShiftJISMapToNativeASCII() {
+function testCharmapShiftJISMapToUnicode() {
 	var cm = new ilib.Charmap({
 		name: "Shift_JIS"
 	});
     assertNotUndefined(cm);
-    var array = cm.mapToNative("This is a test");
-    var expected = [
-        0x54, // T 
-        0x68, // h
-        0x69, // i
-        0x73, // s
-        0x20, // 
-        0x69, // i
-        0x73, // s
-        0x20, // 
-        0x61, // a
-        0x20, //
-        0x74, // t
-        0x65, // e
-        0x73, // s
-        0x74  // t
-    ];
-   
-    for (var i = 0; i < expected.length; i++) {
-    	assertEquals("testing index " + i, expected[i], array[i]);
+    for (var element in testData) {
+	    assertEquals(element, cm.mapToUnicode(testData[element]));
     }
 }
 
-function testCharmapShiftJISMapToNativeHalfWidthKatakana() {
+function testCharmapShiftJISMapToNative() {
 	var cm = new ilib.Charmap({
 		name: "Shift_JIS"
 	});
     assertNotUndefined(cm);
-    
-    var array = cm.mapToNative("ｱｶﾓﾄ");
-    var expected = [
-        0xB1, // ｱ
-        0xB6, // ｶ
-        0xD3, // ﾓ
-        0xC4  // ﾄ
-    ];
-   
-    for (var i = 0; i < expected.length; i++) {
-    	assertEquals("testing index " + i, expected[i], array[i]);
-    }
-}
-
-function testCharmapShiftJISMapToNativeHiragana() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    
-    var array = cm.mapToNative("ひらがなです");
-    var expected = [
-        0x82, 0xD0, // ひ 
-        0x82, 0xE7, // ら 
-        0x82, 0xAA, // が 
-        0x82, 0xC8, // な 
-        0x82, 0xC5, // で 
-        0x82, 0xB7  // す
-    ];
-   
-    for (var i = 0; i < expected.length; i++) {
-    	assertEquals("testing index " + i, expected[i], array[i]);
-    }
-}
-
-function testCharmapShiftJISMapToNativeKatakana() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    
-    var array = cm.mapToNative("カタカナです");
-    var expected = [
-        0x83, 0x4A, // カ
-        0x83, 0x5E, // タ
-        0x83, 0x4A, // カ 
-        0x83, 0x69, // ナ
-        0x82, 0xC5, // で 
-        0x82, 0xB7  // す
-    ];
-   
-    for (var i = 0; i < expected.length; i++) {
-    	assertEquals("testing index " + i, expected[i], array[i]);
-    }
-}
-
-function testCharmapShiftJISMapToNativeGreek() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    
-    var array = cm.mapToNative("Ελλασ");
-    var expected = [
-        0x83, 0xA3, // Ε
-        0x83, 0xC9, // λ
-        0x83, 0xC9, // λ 
-        0x83, 0xBF, // α
-        0x83, 0xD0  // σ 
-    ];
-   
-    for (var i = 0; i < expected.length; i++) {
-    	assertEquals("testing index " + i, expected[i], array[i]);
-    }
-}
-
-function testCharmapShiftJISMapToNativeCyrillic() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    
-    var array = cm.mapToNative("Русский");
-    var expected = [
-        0x84, 0x51, // Р
-        0x84, 0x85, // у
-        0x84, 0x83, // с
-        0x84, 0x83, // с
-        0x84, 0x7B, // к
-        0x84, 0x79, // и
-        0x84, 0x7A  // й
-    ];
-   
-    for (var i = 0; i < expected.length; i++) {
-    	assertEquals("testing index " + i, expected[i], array[i]);
-    }
-}
-
-function testCharmapShiftJISMapToNativeKanji() {
-	var cm = new ilib.Charmap({
-		name: "Shift_JIS"
-	});
-    assertNotUndefined(cm);
-    
-    var array = cm.mapToNative("日本語は美しいです");
-    var expected = [
-		0x93, 0xFA, // 日
-		0x96, 0x7B, // 本
-		0x8c, 0xEA, // 語
-		0x82, 0xCD, // は
-		0x94, 0xFC, // 美
-		0x82, 0xB5, // し
-		0x82, 0xA2, // い
-		0x82, 0xC5, // で
-		0x82, 0xB7  // す
-    ];
-   
-    for (var i = 0; i < expected.length; i++) {
-    	assertEquals("testing index " + i, expected[i], array[i]);
+    for (var element in testData) {
+    	var array = cm.mapToNative(element);
+    	
+        for (var i = 0; i < testData[element].length; i++) {
+        	assertEquals("testing " + element + " index " + i, testData[element][i], array[i]);
+        }
     }
 }
 
