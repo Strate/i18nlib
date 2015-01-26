b206bf38bd16040d3ef045dcc38429fded0d5be9
ehoogerbeets1 <ehoogerbeets1@5ac057f5-ce63-4fb3-acd1-ab13b794ca36>
2015-01-26 22:32:12 +0000

    First build of 10.0

---
+++ tags/20150126-build-10.0-001/js/src/maps/casemapper.js
@@ -0,0 +1,214 @@
+/*
+ * casemapper.js - define upper- and lower-case mapper
+ * 
+ * Copyright © 2014-2015, JEDLSoft
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+// !depends locale.js util/utils.js
+
+/**
+ * @class
+ * Create a new string mapper instance that maps strings to upper or
+ * lower case. This mapping will work for any string as characters 
+ * that have no case will be returned unchanged.<p>
+ * 
+ * The options may contain any of the following properties:
+ * 
+ * <ul>
+ * <li><i>locale</i> - locale to use when loading the mapper. Some maps are 
+ * locale-dependent, and this locale selects the right one. Default if this is
+ * not specified is the current locale.
+ * 
+ * <li><i>direction</i> - "toupper" for upper-casing, or "tolower" for lower-casing.
+ * Default if not specified is "toupper".
+ * </ul>
+ * 
+ * Depends directive: !depends casemapper.js
+ * 
+ * @constructor
+ * @param {Object=} options options to initialize this mapper 
+ */
+ilib.CaseMapper = function (options) {
+	this.up = true;
+	this.locale = new ilib.Locale();
+	
+	if (options) {
+		if (typeof(options.locale) !== 'undefined') {
+			this.locale = (typeof(options.locale) === 'string') ? new ilib.Locale(options.locale) : options.locale;
+		}
+		
+		this.up = (!options.direction || options.direction === "toupper");
+	}
+
+	switch (this.locale.getLanguage()) {
+		case "az":
+		case "tr":
+		case "crh":
+		case "kk":
+		case "krc":
+		case "tt":
+			this.mapData = this.up ?  {
+				"i": "İ",
+				"ı": "I"
+			} : {
+				"İ": "i",
+				"I": "ı"
+			};
+			this.mapper = this._charMapper;
+			break;
+		case "de":
+			if (this.up) {
+				this.mapper = this._charMapper;
+				this.mapData = {
+					"ß": "SS"
+				};
+			}
+			break;
+		case "fr":
+			if (this.up && this.locale.getRegion() !== "CA") {
+				this.mapData = {
+					'à': 'A',
+					'á': 'A',
+					'â': 'A',
+					'ã': 'A',
+					'ä': 'A',
+					'ç': 'C',
+					'è': 'E',
+					'é': 'E',
+					'ê': 'E',
+					'ë': 'E',
+					'ì': 'I',
+					'í': 'I',
+					'î': 'I',
+					'ï': 'I',
+					'ñ': 'N',
+					'ò': 'O',
+					'ó': 'O',
+					'ô': 'O',
+					'ö': 'O',
+					'ù': 'U',
+					'ú': 'U',
+					'û': 'U',
+					'ü': 'U'
+				};
+				this.mapper = this._charMapper;
+			}
+			break;
+		case "el":
+			this.mapData = this.up ? {
+				'ΐ': 'Ι',
+				'ά': 'Α',
+				'έ': 'Ε',
+				'ή': 'Η',
+				'ί': 'Ι',
+				'ΰ': 'Υ',
+				'ϊ': 'Ι',
+				'ϋ': 'Υ',
+				'ό': 'Ο',
+				'ύ': 'Υ',
+				'ώ': 'Ω'	
+			} : {};
+			this.mapper = this._charMapper;
+			break;
+		case "abq":
+		case "ady":
+		case "av":
+		case "ce":
+		case "dar":
+		case "inh":
+		case "kbd":
+		case "lbe":
+		case "lez":
+		case "tab":
+		case "ru":
+			if (!this.up) {
+				this.mapData = {
+					'Ӏ': 'Ӏ'	
+				};
+				this.mapper = this._charMapper;
+			}
+			break;
+	}
+	
+	if (!this.mapper) {
+		this.mapper = function(string) {
+			return this.up ? string.toUpperCase() : string.toLowerCase();
+		};
+	}
+};
+
+ilib.CaseMapper.prototype = {
+	/** 
+	 * @private 
+	 */
+	_charMapper: function(string) {
+		var input;
+		if (!string) {
+			return string;
+		}
+		if (typeof(string) === 'string') {
+			input = new ilib.String(string);
+		} else {
+			input = string.toString();
+		}
+		var ret = "";
+		var it = input.charIterator();
+		var c;
+		
+		while (it.hasNext()) {
+			c = it.next();
+			if (!this.up && c === 'Σ') {
+				if (it.hasNext()) {
+					c = it.next();
+					var code = c.charCodeAt(0);
+					// if the next char is not a greek letter, this is the end of the word so use the
+					// final form of sigma. Otherwise, use the mid-word form.
+					ret += ((code < 0x0388 && code !== 0x0386) || code > 0x03CE) ? 'ς' : 'σ';
+					ret += c.toLowerCase();
+				} else {
+					// no next char means this is the end of the word, so use the final form of sigma
+					ret += 'ς';
+				}
+			} else {
+				if (this.mapData[c]) {
+					ret += this.mapData[c];
+				} else {
+					ret += this.up ? c.toUpperCase() : c.toLowerCase();
+				}
+			}
+		}
+		
+		return ret;
+	},
+
+	/**
+	 * Return the locale that this mapper was constructed with. 
+	 * @returns {ilib.Locale} the locale that this mapper was constructed with
+	 */
+	getLocale: function () {
+		return this.locale;
+	},
+		
+	/**
+	 * Map a string to lower case in a locale-sensitive manner.
+	 * 
+	 * @param {string|undefined} string
+	 * @return {string|undefined}
+	 */
+	map: function (string) {
+		return this.mapper(string);
+	}
+};
